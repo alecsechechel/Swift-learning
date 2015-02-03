6dcad68ecdad62624dd203e58c24c99af4af5f65
Alex <alecsechechel@gmail.com>
2015-02-03 15:24:48 +0200

    HomeWork7

--- HomeWork/Lesson7/Lesson7.playground/section-1.swift
+++ HomeWork/Lesson7/Lesson7.playground/section-1.swift
@@ -4,7 +4,7 @@ import UIKit
 // part1
 let daysInMonths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 let nameMonths   = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-var monthAndDay  = [(String, Int)]()
+var monthAndDay : [(name: String, day: Int)] = []
 
 for var i = 0; i < nameMonths.count; i++ {
     
@@ -32,7 +32,7 @@ println()
 
 for tupl in monthAndDay {
     
-    println("\(tupl.0) has \(tupl.1) days!")
+    println("\(tupl.name) has \(tupl.day) days!")
 }
 
 println()
@@ -41,28 +41,17 @@ for var i = daysInMonths.count - 1; i >= 0; i-- {
     
     println(daysInMonths[i])
 }
+println()
 
-let randomMonth = Int(arc4random_uniform(11) + 1)
-var randomDay   = 0
-
-if randomMonth == 2 {
-    randomDay == Int(arc4random_uniform(27) + 1)
-} else if randomMonth == 4 || randomMonth == 6 || randomMonth == 9 || randomMonth == 11 {
-    randomDay = Int(arc4random_uniform(29) + 1)
-} else {
-    randomDay = Int(arc4random_uniform(30) + 1)
-}
-
-let randomDate = (month: randomMonth, day: randomDay)
-randomDate.month
-randomDate.day
-
-var countDay   = 0
+let randomMonth = Int(arc4random_uniform(12) + 1)
+var randomDay   = Int(arc4random_uniform(UInt32(daysInMonths[randomMonth - 1])) + 1)
+let randomDate  = (month: randomMonth, day: randomDay)
+var countDay    = 0
 
 for var i = 0; i < randomDate.month - 1; i++ {
     countDay += daysInMonths[i]
 }
-println("Days from \(randomDate.month).\(randomDate.day) = \(countDay + randomDate.day)")
+println("Days from \(randomDate.month).\(randomDate.day) = \(countDay + randomDate.day - 1)\n")
 
 //part 2
 let optionalArray = ["5", "3c", "10", "12", "one"]
@@ -94,12 +83,13 @@ sumArray
 
 //part3
 let alphabet      = "abcdefghijklmnopqrstuvwxyz"
+let alphabetArray2 = map(reverse(alphabet)) { s -> String in String(s) }
 var alphabetArray = [String]()
-
 for char in alphabet {
     alphabetArray.insert(String(char), atIndex: 0)
 }
 println(alphabetArray)
+println(alphabetArray2)
 
 
 
