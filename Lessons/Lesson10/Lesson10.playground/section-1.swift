1db1b238553881f7ab59dceb75c11b5d4d97063b
Alex <alecsechechel@gmail.com>
2015-02-16 18:48:31 +0200

    home work 10

--- Lessons/Lesson10/Lesson10.playground/section-1.swift
+++ Lessons/Lesson10/Lesson10.playground/section-1.swift
@@ -1,1 +1,57 @@
-// Lesson 10 Function
+// Lesson 10 Function DRY
+
+func calculateMoney(inWallet wallet: [Int], type: Int? = nil) -> (sum:Int, count:Int) {
+    
+    var sum = 0
+    var count = 0
+    
+    for value in wallet {
+
+        if  (type == nil) || (value == type!) {
+            sum += value
+            count++
+        }
+    }
+    
+    return (sum, count)
+}
+
+func calculateMoney(inSequance range: Int...) -> Int {
+    
+    var sum = 0
+    
+    for value in range {
+        sum += value
+    }
+    
+    return sum
+}
+
+func sayHi() {
+    println("Hi")
+}
+
+func sayPhrase(phrase: String) -> Int? {
+    println(phrase)
+    return 0
+}
+
+func doSomething(whatToDo:()->()) {
+    whatToDo()
+}
+
+let wallet = [100, 5, 1, 5, 5, 20, 50, 100, 1, 1]
+
+
+
+let sum = calculateMoney(inWallet: wallet, type: 100)
+sum.sum
+calculateMoney(inWallet: wallet, type: 100).count
+
+calculateMoney(inSequance: 5,7,100,2)
+let hi : () -> () = sayHi
+hi()
+let phrase = sayPhrase
+phrase("bbb")
+
+
